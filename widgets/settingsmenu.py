eda89eef5ef8165888dba0ca5b9cd763aba53bf8
unknown <ricardo98@gmail.com>
2021-06-03 18:09:14 +0200

    made a setting input and function to be executed when saving settings

--- widgets/settingsmenu.py
+++ widgets/settingsmenu.py
@@ -22,6 +22,11 @@ class SettingsMenu:
             self.opened = False
             self.frame.place_forget()
 
+    def submit_entries(self, stringvars):
+        print(stringvars.get())
+
+    def empty_entries(self, stringvars):
+        print(stringvars.get())
 
     def setup_window(self):
         self.setup_settings()
@@ -33,13 +38,22 @@ class SettingsMenu:
     def setup_settings(self):
         self.frame = tk.Frame(self.window, width=self.window.winfo_width(), height=self.window.winfo_height())
 
-        color_frame = tk.Frame(self.window, width=80 + 10 + 150, height=25)
+        color_frame = tk.Frame(self.frame, width=80 + 10 + 80, height=50,)
 
         sv_ec_entry = tk.StringVar(name="EC_ENTRY")
-        ec_label = tk.Label(color_frame, text="Empty color:", anchor='w')
-        ec_label.place(x=0, y=25 + 5, width=80, height=25)
+        ec_label = tk.Label(color_frame, text="Empty Color:", anchor='w')
+        ec_label.place(x=10, y=10, width=80, height=25)
         # Snipe Time entry
-        ec_entry = tk.Entry(color_frame, textvariable=sv_ec_entry, width=150)
-        ec_entry.place(in_=ec_label, relx=1.0, x=10)
+        ec_entry = tk.Entry(color_frame, textvariable=sv_ec_entry, width=80)
+        ec_entry.place(in_=ec_label, relx=1.0, x=10, height=25)
 
-        # return settings_frame
+        color_frame.place(x=0, y=0)
+
+
+        btn_frame = tk.Frame(self.frame, width=100, height=30)
+        submit_btn = tk.Button(btn_frame, text='Save', command=lambda: self.submit_entries(sv_ec_entry))
+        submit_btn.place(x=0, y=0, width=50, height=28)
+        empty_btn = tk.Button(btn_frame, text='Empty', command=lambda: self.empty_entries(sv_ec_entry))
+        empty_btn.place(x=50, y=0, width=50, height=28)
+
+        btn_frame.place(x=290, y=110)
